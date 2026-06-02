56c5f709aabd00a52e0d71adeee26b6b0f4755eb
visatk <visatk29@gmail.com>
2026-06-03 01:32:32 +0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,9 @@ export { validateLuhn } from './luhn';
 export { detectNetwork } from './network';
 export { formatCardNumber } from './formatter';
 export { getTestCard, generateRandomCard } from './generator';
-export { lookupBin } from './binlist';
 export { validateCardStrict } from './validator';
-export { EXTENDED_BINS } from './bin-ranges'; // New export
+export { EXTENDED_BINS } from './bin-ranges';
 
 // Exporting Types
-export type { BinLookupResponse } from './binlist';
 export type { CardNetwork, TestScenario, CardInfo } from './data';
 export type { CardValidationResult } from './validator';
