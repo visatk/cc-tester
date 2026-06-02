813322e20e55459fb3198dfb30dafce5d3b48703
visatk <visatk29@gmail.com>
2026-06-03 00:04:55 +0600

    Update validator.ts

---
+++ src/validator.ts
@@ -0,0 +1,23 @@
+export function validateLuhn(cardNumber: string): boolean {
+  const sanitized = cardNumber.replace(/\D/g, '');
+  if (!sanitized || sanitized.length < 13 || sanitized.length > 19) {
+    return false;
+  }
+
+  let sum = 0;
+  let shouldDouble = false;
+
+  for (let i = sanitized.length - 1; i >= 0; i--) {
+    let digit = parseInt(sanitized.charAt(i), 10);
+
+    if (shouldDouble) {
+      digit *= 2;
+      if (digit > 9) digit -= 9;
+    }
+
+    sum += digit;
+    shouldDouble = !shouldDouble;
+  }
+
+  return sum % 10 === 0;
+}
