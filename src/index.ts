386b1a304028b9e4079baf9e019b4f5bea0ebb94
visatk <visatk29@gmail.com>
2026-06-03 01:00:44 +0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export { formatCardNumber } from './formatter';
 export { getTestCard, generateRandomCard } from './generator';
 export { lookupBin } from './binlist';
 export { validateCardStrict } from './validator';
+export { EXTENDED_BINS } from './bin-ranges'; // New export
 
 // Exporting Types
 export type { BinLookupResponse } from './binlist';
