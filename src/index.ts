18a6d1a558bd1beb78a63a3df29841e205afd9d9
visatk <visatk29@gmail.com>
2026-06-03 00:12:20 +0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { validateLuhn } from './luhn';
 export { detectNetwork } from './network';
 export { getTestCard } from './generator';
+export { formatCardNumber } from './formatter';
 export type { CardNetwork, TestScenario, CardInfo } from './data';
